41ee3e9b7aacfb484dd906a80feca8de0dc102dc
LuiFig19 <lui@fanaticnode.com>
2025-08-23 14:37:06 -0400

    Stabilize root page generation: make root index static wrapper around marketing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,8 @@
-export { default } from './(marketing)/page'
+export const dynamic = 'force-static'
+export const revalidate = 0
+
+import LandingPage from './(marketing)/page'
+
+export default function RootIndex() {
+  return <LandingPage />
+}
